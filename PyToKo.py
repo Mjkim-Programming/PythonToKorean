57299637cf331bb76ca8811e45b21b621941c6b2
Kim min-jae <mjkim11084@gmail.com>
2024-10-11 11:58:12 +0900

    #1 PyToKo a0.0.1

---
+++ PyToKo.py
@@ -0,0 +1,18 @@
+def 출력하다(입력값):
+    print(입력값)
+def 타입(입력값):
+    return type(입력값)
+def 입력받다_문자열과함께(문자열):
+    return input(문자열)
+def 입력받다_문자열없이():
+    return input()
+def 정수(입력값):
+    return int(입력값)
+def 실수(입력값):
+    return float(입력값)
+def 문자열(입력값):
+    return str(입력값)
+def 참():
+    return True;
+def 거짓():
+    return False;
